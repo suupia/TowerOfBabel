9f93f9fee0d609c774fb5ed6f4b14422993afdce
DAIV_Z9\user <morita777k@icloud.com>
2022-09-05 18:48:15 +0900

    デバッグとして座標の値をUnity上に表示できるようにした。

--- Assets/Scripts/0__GameManager/DebugMGR.cs
+++ Assets/Scripts/0__GameManager/DebugMGR.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class DebugMGR : MonoBehaviour
 {
+    [SerializeField] GameObject debugCanvas;
     [SerializeField] GameObject debugMapTextParent;
     GameObject[] debugMapTextRows;
 
@@ -18,6 +19,7 @@ public class DebugMGR : MonoBehaviour
         Debug.Log($"DebugMapCoordinateを実行します");
         int mapWidth = GameManager.instance.battleMGR.mapMGR.GetMapWidth();
         int mapHeight = GameManager.instance.battleMGR.mapMGR.GetMapHeight();
+        GameObject textGO;
 
         //行でループのあと、列のループをする
         for(int i = 0; i < mapWidth; i++)
@@ -25,7 +27,22 @@ public class DebugMGR : MonoBehaviour
             for(int j = 0; j < mapHeight; j++)
             {
                 Debug.Log($"i:{i}, j:{j}");
-                debugMapTextParent.transform.GetChild(i).GetChild(j).gameObject.GetComponent<TextMeshProUGUI>().text = $"({i}, {j})";
+                textGO = debugMapTextParent.transform.GetChild(i).GetChild(j).gameObject;
+
+                //textの内容を変更
+                textGO.GetComponent<TextMeshProUGUI>().text = $"({i}, {j})";
+
+                //textの位置を変更
+                textGO.GetComponent<RectTransform>().position = RectTransformUtility.WorldToScreenPoint(Camera.main, new Vector3(i+0.5f, j+0.5f, 0));
+
+
+                //Vector2Int vector = new Vector2Int(j, j);
+                //var newPos = Vector2.zero;
+                //var screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, new Vector3(vector.x, vector.y,0));
+                //RectTransformUtility.ScreenPointToLocalPointInRectangle(debugCanvas.GetComponent<RectTransform>(), screenPos, Camera.main, out newPos);
+                //textGO.GetComponent<RectTransform>().localPosition = newPos;
+
+
             }
         }
 
